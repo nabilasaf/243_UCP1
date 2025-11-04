4ef7f4362c17a28bd11075d5b97e28ddc0ed4e76
nabilasaf <nabilasafitri861@gmail.com>
2025-11-04 09:31:56 +0700

    menambahkan put untuk film

--- models/index.js
+++ models/index.js
@@ -25,3 +25,18 @@ app.get('/film',async (req, res) => {
         res.send(err);
     }
   });
+
+  app.put('/film/:id', async (req, res) => {
+    const filmId = req.params.id;
+    const data = req.body;
+    try {
+        const film = await db.Hollywood.findByPk(komikId);
+        if (!film) {
+            return res.status(404).send({ message: 'Film tidak ditemukan' });
+        }
+        await film.update(data);
+        res.send({message: 'Film berhasil diupdate', komik});
+    } catch (err) {
+        res.status(500).send(err);
+    }
+  });
